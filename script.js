46a7227a6d69510f670ea9f220e83c6c88a4ccc2
Márcio Diniz <marcio.sccp10@gmail.com>
2022-10-25 15:56:14 -0300

    Criação Inicial

---
+++ script.js
@@ -0,0 +1,10 @@
+const clicou1 = document.querySelector('.btn');
+const clicou2 = document.querySelector('.botao');
+
+clicou1.addEventListener('click', () => {
+    alert('Clicou no botão Bootstrap!');
+})
+
+clicou2.addEventListener('click', () => {
+    alert('Clicou no botão CSS!');
+})
